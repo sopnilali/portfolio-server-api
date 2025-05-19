37f1a2035fee162043fa7a48d18b391028c7a1a3
sopnilali <sopnilali24@gmail.com>
2025-05-19 21:46:02 +0600

    main portfolio link added from midleware

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app = express()
 
 // CORS configuration
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://my-portfolio-dashboard-nine.vercel.app'],
+    origin: ['http://localhost:3000', 'https://my-portfolio-dashboard-nine.vercel.app', 'https://mdabduladudui.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
